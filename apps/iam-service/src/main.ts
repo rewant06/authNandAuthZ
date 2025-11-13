17079ba1924054ba00a6509de4e4c210209231e9
Rewant Raj <2004125@kiit.ac.in>
2025-11-13 22:40:19 +0530

    iam-service: added cookie-parser

--- apps/iam-service/src/main.ts
+++ apps/iam-service/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('trust proxy', 1);
+
+  app.use(cookieParser());
 
   app.enableCors({
     origin: (origin, callback) => {
