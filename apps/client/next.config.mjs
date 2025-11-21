48d98fb85bbfc53597ca2c5f555c2cb37731606d
Rewant Raj <2004125@kiit.ac.in>
2025-11-21 18:12:27 +0530

    client: nextconfig default url changed

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    const backendUrl = process.env.BACKEND_URL || "http://localhost:8000";
+    const backendUrl = process.env.BACKEND_URL || "https://api.helpingbots.in";
 
 
     return [
